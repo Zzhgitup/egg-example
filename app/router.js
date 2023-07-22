fed9be191fa512c5e08703dd7bf5b1e7872f69a4
Zzhgitup <99540215+Zzhgitup@users.noreply.github.com>
2023-07-22 11:24:34 +0800

    部分修改

--- app/router.js
+++ app/router.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = (app) => {
   const { router, controller } = app;
-  router.get("/", controller.home.index);
-  router.get("/user", controller.home.user); //占位符params
-  router.post("/adduser", controller.home.adduser);
-  router.post("/edior", controller.home.ediruser);
-  router.delete("/deleteuser", controller.home.deleteuser);
+  router.get('/', controller.home.index);
+  router.get('/user', controller.home.user); // 占位符params
+  router.post('/adduser', controller.home.adduser);
+  router.post('/edior', controller.home.ediruser);
+  router.delete('/deleteuser', controller.home.deleteuser);
 };
